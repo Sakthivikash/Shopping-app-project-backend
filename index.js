921cfaa7d57da5a8595cf1fd984bf36798c432dc
Sakthivikash <sakthivikash70@gmail.com>
2022-08-16 22:00:51 +0530

    updated port

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ mongoose.connect(`${process.env.MONGO_URL}`, () =>
 mongoose.connection.on("error", (err) => {
   console.log(err);
 });
-
-server.listen(5000, () => {
-  console.log("Server is running at port 5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
 });
 app.set("socketio", io);
